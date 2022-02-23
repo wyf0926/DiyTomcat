ff11ac1328795e3433c595d0215e17f55a7c3395
wyf0926 <wuyifan0926>
2022-02-23 00:03:02 -0500

    modify git ignore & add tests

---
+++ src/com/wyf/diytomcat/test/TestTomcat.java
@@ -0,0 +1,35 @@
+package com.wyf.diytomcat.test;
+
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.StrUtil;
+import com.wyf.diytomcat.util.MiniBrowser;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestTomcat {
+    private static int port = 18080;
+    private static String ip = "127.0.0.1";
+    @BeforeClass
+    public static void beforeClass() {
+        //所有测试开始前看diy tomcat 是否已经启动了
+        if(NetUtil.isUsableLocalPort(port)) {
+            System.err.println("请先启动 位于端口: " +port+ " 的diy tomcat，否则无法进行单元测试");
+            System.exit(1);
+        }
+        else {
+            System.out.println("检测到 diy tomcat已经启动，开始进行单元测试");
+        }
+    }
+
+    @Test
+    public void testHelloTomcat() {
+        String html = getContentString();
+        Assert.assertEquals(html,"Hello DIY Tomcat from how2j.cn");
+    }
+
+    private String getContentString() {
+        String url = StrUtil.format("http://{}:{}{}", ip,port, "/");
+        return MiniBrowser.getContentString(url);
+    }
+}
